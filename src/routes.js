92276571fd9a39947d316ec300b5dbe5f19520d0
Felipe <felipe.almeidaa14@gmail.com>
2020-05-29 22:08:43 -0300

    inserindo avatar de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,21 @@
 import Router from 'express';
+import Multer from 'multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import authMiddlewares from './app/middlewares/auth';
-
+import multerConfig from './config/multer';
+import FileController from './app/controller/FileController';
 
 const routes = new Router();
-
+const upload = Multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewares);
 
-routes.put('/users', UserController.update)
+routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
