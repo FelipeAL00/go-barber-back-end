00ac73fee4e4aee33edb9039c5e4cdc30e2b899c
Felipe <felipe.almeidaa14@gmail.com>
2020-06-10 21:12:33 -0300

    adicionando criação de notificação com mongodb

--- src/app/controller/AppointmentController.js
+++ src/app/controller/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../model/Appointment';
 import User from '../model/User';
 import File from '../model/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async store(req, res) {
@@ -52,6 +54,20 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM',' 'às' H:mm'h'",
+      {
+        locale: pt,
+      }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate} `,
+      user: provider_id,
     });
 
     return res.json(appointment);
